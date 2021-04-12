81952492d7dfb53d701ddbc04a9d4b7200aa6416
Reyu Hero <reyu443@gmail.com>
2021-04-12 23:07:01 +0430

    removed frontend model

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -12,7 +12,7 @@ class Admin extends CI_Controller
         $this->load->database();
         $this->load->library('session');
         $this->load->model('crud_model');
-        $this->load->model('frontend_model');
+        // $this->load->model('frontend_model');
         
         // cache control
         $this->output->set_header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
